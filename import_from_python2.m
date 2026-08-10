ef851ecceefea85dcaa9b94dfec883a5a9354a98
auriane-str <auriane.streibig@polymtl.ca>
2026-08-10 14:07:00 -0400

    Update import_from_python2.m

--- import_from_python2.m
+++ import_from_python2.m
@@ -1,13 +1,13 @@
-pyenv( "Version", "/home/auristre/.conda/envs/SpikeNet2/bin/python")
+pyenv( "Version", "your_conda_environment")
 
-dossierProjet = "/NAS/home/auristre/Documents/software/SpikeNet2/";
+dossierProjet = "your_path";
 
 cd(dossierProjet);
 
-fichierMat = "/NAS/home/auristre/Documents/software/SpikeNet2/data/PA07_05bis_reorderd.mat";
+fichierMat = "yout_data_path.mat";
 
-commande = sprintf(['/home/auristre/.conda/envs/SpikeNet2/bin/python ' ...
-    '/NAS/home/auristre/Documents/software/SpikeNet2/for_matlab.py "%s"'], ...
+commande = sprintf(['your_conda_environment' ...
+    'your_path/for_matlab.py "%s"'], ...
     fichierMat);
 
 system(commande);
