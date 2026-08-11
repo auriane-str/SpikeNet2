738e04bebe2e00ff72dde20833f6df8e4e5975be
auriane-str <auriane.streibig@polymtl.ca>
2026-08-11 15:45:59 -0400

    Add files via upload

--- for_matlab.py
+++ for_matlab.py
@@ -1,108 +1,115 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
-import sys
+# In[ ]:
+
+
+from torch.utils.data import DataLoader
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import os
+import pickle
+from torchvision import transforms
 import pytorch_lightning as pl
 import torch
-from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+# load own code
+import sys
 sys.path.append('../')
 from sleeplib.Resnet_15.model import ResNet
-from sleeplib.datasets import ContinousToSnippetDataset
-from sleeplib.montages import con_ECG_combine_montage
+from sleeplib.datasets import BonoboDataset, ContinousToSnippetDataset
+# this holds all the configuration parameters
 from sleeplib.config import Config
+import pickle
 
+from pytorch_lightning.callbacks import ModelCheckpoint
+from torch.utils.data import DataLoader
+from torchvision import transforms
 
+from sleeplib.datasets import BonoboDataset , ContinousToSnippetDataset
+from sleeplib.montages import CDAC_bipolar_montage,CDAC_common_average_montage,CDAC_combine_montage,con_combine_montage, con_ECG_combine_montage
+from sleeplib.transforms import cut_and_jitter, channel_flip,extremes_remover
 
-# Configuration
-
+# load config and show all default parameters
 config = Config()
-path_model = 'your_path/SpikeNet2/'
+path_model = '/NAS/home/auristre/Documents/software/SpikeNet2/'
 
-Batch_size = 128
-Fs = config.FQ
-Window_size = config.WINDOWSIZE
-step = 32
-
-# load montage
+# set up dataloader to predict all samples in test dataset
 transform_train = transforms.Compose([extremes_remover(signal_max = 2000, signal_min = 20)])
 con_combine_montage = con_ECG_combine_montage()
 
 
 # load pretrained model
-model = ResNet.load_from_checkpoint('yout_path/model/new_weights.ckpt',
+model = ResNet.load_from_checkpoint('/NAS/home/auristre/Documents/software/SpikeNet2/model/new_weights.ckpt',
                                         lr=config.LR,
                                         n_channels=37,
                                         map_location=torch.device('cpu') ,
                                        )
-                                        
+                                        #map_location=torch.device('cpu') add this if running on CPU machine
 
-trainer = pl.Trainer(
-  devices=1,
-  accelerator="gpu",
-  fast_dev_run=False,
-  enable_progress_bar=False
-)
+    
+# init trainer
+#trainer = pl.Trainer(fast_dev_run=False,enable_progress_bar=False,devices = 1,strategy ='ddp')
+trainer = pl.Trainer(devices=1, accelerator="gpu",fast_dev_run=False,enable_progress_bar=False)
 
 # store results
-path_controls = os.path.join("your_path/SpikeNet2/controlset.csv")
+path_controls = os.path.join("/NAS/home/auristre/Documents/software/SpikeNet2/controlset.csv")
 
 controls = pd.read_csv(path_controls)
 i = 0
+#controls = controls[controls['Mode']=='Test']
 
+path = sys.argv[1]
 
+import scipy.io as sio
+Bonobo_con = ContinousToSnippetDataset(
+    path,
+    montage=con_combine_montage,
+    transform=None,
+    Fq=128,
+    window_size=1,
+    step=32
+)
 
-for eeg_file in tqdm(controls.EEG_index):
-    
-    path = sys.argv[1]
-    
-    Bonobo_con = ContinousToSnippetDataset(
-      path,
-      montage=con_combine_montage,
-      transform=None,
-      window_size=config.WINDOWSIZE
-    )
-  
-    con_dataloader = DataLoader(
-      onobo_con, 
-      batch_size=128,
-      shuffle=False,
-      num_workers=os.cpu_count()
-    )
- 
-    preds = trainer.predict(model,con_dataloader)
-    
-    preds = np.concatenate(preds).astype(float)
+print("Number of windows:", len(Bonobo_con))
 
-    preds = pd.DataFrame(preds)
-    preds.to_csv(path + '_predictions2' +'.csv',index=False)
-    
-    
+con_dataloader = DataLoader(
+    Bonobo_con,
+    batch_size=128,
+    shuffle=False,
+    num_workers=4
+)
 
-# path to csv file
+preds = trainer.predict(model, con_dataloader)
+preds = np.concatenate(preds).astype(float).squeeze()
+
+sio.savemat(path + 'predictions2.mat',{'preds': preds})
+    
+    
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# chemin vers ton fichier csv
 path_pred = path + '_predictions2.csv'
 
-# load predictions
+# charger les prédictions
 preds = pd.read_csv(path_pred)
 
-# convert into numpy
+# convertir en numpy
 preds = preds.values.squeeze()
 
-# predictions frequency
+# fréquence des prédictions (à adapter)
+# si une prédiction = une fenêtre de 1 seconde :
 fs_pred = 128  
 
 # axe temporel
 step=32
-#time = np.arange(len(preds)) *step / fs_pred
-time = np.arange(len(preds))
+time = np.arange(len(preds)) *step / fs_pred
+#time = np.arange(len(preds))
 
-# Visualization
-
+# plot
 plt.figure(figsize=(12,4))
 plt.plot(time, preds)
 
