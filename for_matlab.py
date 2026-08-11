1953cffe16320ba117f161f0a4d0242b9d4475de
auriane-str <auriane.streibig@polymtl.ca>
2026-08-11 11:23:54 -0400

    Update for_matlab.py

--- for_matlab.py
+++ for_matlab.py
@@ -1,39 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from torch.utils.data import DataLoader
+import os
+import sys
 import numpy as np
 import pandas as pd
-import os
-import pickle
-from torchvision import transforms
+import matplotlib.pyplot as plt
 import pytorch_lightning as pl
 import torch
+from torch.utils.data import DataLoader
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 
-# load own code
-import sys
 sys.path.append('../')
 from sleeplib.Resnet_15.model import ResNet
-from sleeplib.datasets import BonoboDataset, ContinousToSnippetDataset
-# this holds all the configuration parameters
+from sleeplib.datasets import ContinousToSnippetDataset
+from sleeplib.montages import con_ECG_combine_montage
 from sleeplib.config import Config
-import pickle
 
-from pytorch_lightning.callbacks import ModelCheckpoint
-from torch.utils.data import DataLoader
-from torchvision import transforms
 
-from sleeplib.datasets import BonoboDataset , ContinousToSnippetDataset
-from sleeplib.montages import CDAC_bipolar_montage,CDAC_common_average_montage,CDAC_combine_montage,con_combine_montage, con_ECG_combine_montage
-from sleeplib.transforms import cut_and_jitter, channel_flip,extremes_remover
 
-# load config and show all default parameters
+# Configuration
+
 config = Config()
 path_model = 'your_path/SpikeNet2/'
 
-# set up dataloader to predict all samples in test dataset
+Batch_size = 128
+Fs = config.FQ
+Window_size = config.WINDOWSIZE
+step = 32
+
+# load montage
 transform_train = transforms.Compose([extremes_remover(signal_max = 2000, signal_min = 20)])
 con_combine_montage = con_ECG_combine_montage()
 
@@ -44,35 +40,44 @@ model = ResNet.load_from_checkpoint('yout_path/model/new_weights.ckpt',
                                         n_channels=37,
                                         map_location=torch.device('cpu') ,
                                        )
-                                        #map_location=torch.device('cpu') add this if running on CPU machine
+                                        
 
-    
-# init trainer
-trainer = pl.Trainer(devices=1, accelerator="gpu",fast_dev_run=False,enable_progress_bar=False)
+trainer = pl.Trainer(
+  devices=1,
+  accelerator="gpu",
+  fast_dev_run=False,
+  enable_progress_bar=False
+)
 
 # store results
 path_controls = os.path.join("your_path/SpikeNet2/controlset.csv")
 
 controls = pd.read_csv(path_controls)
 i = 0
-#controls = controls[controls['Mode']=='Test']
 
 
-import scipy.io as sio
+
 for eeg_file in tqdm(controls.EEG_index):
-    
-    import sys
-
     
     path = sys.argv[1]
     
-    Bonobo_con = ContinousToSnippetDataset(path,montage=con_combine_montage,transform=None,window_size=config.WINDOWSIZE)
-    con_dataloader = DataLoader(Bonobo_con, batch_size=128,shuffle=False,num_workers=os.cpu_count())
+    Bonobo_con = ContinousToSnippetDataset(
+      path,
+      montage=con_combine_montage,
+      transform=None,
+      window_size=config.WINDOWSIZE
+    )
+  
+    con_dataloader = DataLoader(
+      onobo_con, 
+      batch_size=128,
+      shuffle=False,
+      num_workers=os.cpu_count()
+    )
  
     preds = trainer.predict(model,con_dataloader)
     
-    preds = np.concatenate(preds)
-    preds = preds.astype(float)
+    preds = np.concatenate(preds).astype(float)
 
     preds = pd.DataFrame(preds)
     preds.to_csv(path + '_predictions2' +'.csv',index=False)
@@ -96,7 +101,8 @@ step=32
 #time = np.arange(len(preds)) *step / fs_pred
 time = np.arange(len(preds))
 
-# plot
+# Visualization
+
 plt.figure(figsize=(12,4))
 plt.plot(time, preds)
 
